30bfd01533e17da893d8f8b077fbe774144445c8
Nicolas Colomer <ncolomer@octo.com>
2012-07-16 23:19:10 +0200

    Initial include of project's files

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Nicolas Colomer"
+maintainer_email "ncolomer@ymail.com"
+license          "Apache 2.0"
+description      "Installs/Configures OpenStreetMap Osmosis tool"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
---
+++ recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: osmosis
+# Recipe:: default
+#
+
+#include_recipe "ark"
+
+OSMOSIS_HOME = "#{node[:osmosis][:path]}/osmosis"
+
+# Install Osmosis
+ark "osmosis" do
+  url "#{node[:osmosis][:url]}"
+  checksum "#{node[:osmosis][:checksum]}"
+  version "#{node[:osmosis][:version]}"
+  append_env_path true
+  prefix_root "#{node[:osmosis][:path]}"
+  prefix_home "#{node[:osmosis][:path]}"
+  has_binaries [ 'bin/osmosis' ]
+  action :install
+end
+
+# Create the Osmosis Java option file
+file "/etc/osmosis" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+  content "JAVACMD_OPTIONS=\"#{node[:osmosis][:java_opts]}\""
+end
+
+# Create the plugin configuration file
+file "#{OSMOSIS_HOME}/config/osmosis-plugins.conf" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+  content node[:osmosis][:plugins].collect{|plugin| "#{plugin["class_name"]}"}.join("\n")
+end
+
+# Put all plugin jars in the $OSMOSIS_HOME/lib/default/ directory
+for plugin in node[:osmosis][:plugins]
+  remote_file "#{OSMOSIS_HOME}/lib/default/#{plugin["jar_name"]}" do
+    source "#{plugin["jar_url"]}"
+    owner "root"
+    group "root"
+    mode 0755
+  end
+end
