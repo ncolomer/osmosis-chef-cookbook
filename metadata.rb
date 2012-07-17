044f58ff90418db33b9174469111fea6c3691a23
Nicolas Colomer <ncolomer@octo.com>
2012-07-17 07:50:47 +0200

    Add the dependency on Ark provider

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,6 @@ license          "Apache 2.0"
 description      "Installs/Configures OpenStreetMap Osmosis tool"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+depends          "ark"
+recipe           "osmosis", "Installs Osmosis"
