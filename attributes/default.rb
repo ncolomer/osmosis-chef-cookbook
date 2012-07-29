4f8a831803ce7b647a0883a226a34774c5fd673c
Nicolas Colomer <ncolomer@octo.com>
2012-07-29 11:25:07 +0200

    Down default JVM max heap size to 512Mo

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ set[:osmosis][:version] = "0.40.1"
 set[:osmosis][:checksum] = "3f4e29184341fbe8996f6dcd70d5c8104cab2a3c643ae4ca6d5389fc232a2d00"
 
 set[:osmosis][:path] = "/opt"
-set[:osmosis][:java_opts] = "-server -Xmx2G"
+set[:osmosis][:java_opts] = "-server -Xmx512m"
